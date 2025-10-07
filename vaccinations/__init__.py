5b58e7f363f051fe40cd38901d6f91b7375b89bb
Bharti Dhote <152138758+bharti78@users.noreply.github.com>
2025-10-07 21:23:58 +0530

    Delete vaccinations directory

--- vaccinations/__init__.py
+++
@@ -1,3 +0,0 @@
-default_app_config = "vaccinations.apps.VaccinationsConfig"
-
-
--- vaccinations/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class VaccinationsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "vaccinations"
-    def ready(self):
-        from . import signals  # noqa
